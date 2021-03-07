158d60ede248e10dc80ff9a10bb03cd88582d224
costin <2398925401@qq.com>
2021-03-07 22:04:57 +0800

    second commit

---
+++ javatest01/src/index.java
@@ -0,0 +1,9 @@
+public class index {
+    public static void main(String[] args) {
+        int a = 0;
+        for (int i = 0; i < 10; i++){
+            a += i;
+            System.out.print(a + " ");
+        }
+    }
+}
